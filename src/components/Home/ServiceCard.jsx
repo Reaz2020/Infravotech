15a4a2a53044b0da33bd4497365cfc486b272966
Reaz2020 <reaz.morshed@yahoo.com>
2025-05-12 09:06:00 +0200

    updted nme

--- src/components/Home/ServiceCard.jsx
+++ src/components/Home/ServiceCard.jsx
@@ -26,7 +26,7 @@ const ServiceList = () => {
     return (
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
             <ServiceCard
-                title="Web Design"
+                title="Web Design "
                 description="We create visually appealing and user-friendly website designs that capture your brand's identity."
                 icon={<FaDesktop />}
                 link="/services/web-design"
